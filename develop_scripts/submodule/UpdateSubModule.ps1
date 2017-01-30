a25c854543d0e9c3188aa862633c430f9bad2497
MaLiN2223 <szyszka45@gmail.com>
2017-01-30 18:42:49 +0100

    update

---
+++ develop_scripts/submodule/UpdateSubModule.ps1
@@ -0,0 +1,18 @@
+﻿write-host "Starting ..." 
+$toIgnore = ".git","README.md"
+$scriptDirectory = $PSScriptRoot
+cd ../..
+$submoduleDirectory = "py_proj_submodule\"
+$destinationDirectory = "./"
+$files = get-childitem  -path $submoduleDirectory
+write-host "Current directory =" $directory
+
+foreach ($file in $files)
+{
+   if ($file.Name -in $toIgnore){
+        write-host "Found " $file.Name "Ignoring..."
+   } 
+   else{
+    Copy-Item -path $file.FullName -dest $destinationDirectory -recurse -Force
+   }
+}
---
+++ develop_scripts/submodule/updatesubmodule.bat
@@ -0,0 +1,4 @@
+cd ../..
+git submodule update
+cd develop_scripts/submodule/
+PowerShell.exe -file UpdateSubMOdule.ps1
